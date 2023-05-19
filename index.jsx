82e8a03a5d0a1ebaa33aec0d9da8c98a681e744b
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-05-19 10:54:05 +0100

    update-index

--- index.jsx
+++ index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {  Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+//import {  Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import App from './App'
 import { ThemeContextProvider } from "./useThemeContext";
 
-const AppRouter = createBrowserRouter(createRoutesFromElements(
- <Route path="/" element={<App />}  />
-))
+// const AppRouter = createBrowserRouter(createRoutesFromElements(
+//  <Route path="/" element={<App />}  />
+// ))
 
-function Apps() {
-  return <RouterProvider router={AppRouter} />
-}
+// function Apps() {
+//   return <RouterProvider router={AppRouter} />
+// }
 
 ReactDOM.createRoot(
   document.getElementById("root")).render(
