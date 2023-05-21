ef09db42fc98c2e27b421619b08fd1902d64ee52
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-05-21 17:08:09 +0100

    remove caode

--- useThemeContext.jsx
+++ useThemeContext.jsx
@@ -4,11 +4,13 @@ const ThemeContext = createContext()
 
 function ThemeContextProvider(props) {
     const [theme, setTheme] = useState(() => {
+        // Retrieve the theme value from local storage or default to 'light'
         const storeTheme = localStorage.getItem('theme')
         return storeTheme ? storeTheme : 'light'
     })
 
     useEffect(() =>{
+         // Save the current theme value to local storage whenever it changes
         localStorage.setItem('theme', theme)
     },[theme])
     
@@ -17,6 +19,7 @@ function ThemeContextProvider(props) {
     }
 
     return(
+        //render props  
         <ThemeContext.Provider value={{theme, toggleTheme}}>
             {props.children}
         </ThemeContext.Provider>
